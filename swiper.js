b820c16b5265b7fd7595416fb89356b2f63473cd
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-02-28 22:45:36 +0530

    update

--- swiper.js
+++ swiper.js
@@ -7,3 +7,13 @@ let swiper=new Swiper(".swiper",{
         delay:2000,
     }
 })
+// =========================sing page html===========================
+let abc=new Swiper("#ani",{
+    navigation:{
+        nextE1:".swiper-button-next",
+        prevE1:".swiper-button-prev"
+    },
+    autoplay:{
+        delay:2000,
+    }
+})
---
+++ vanta.js
@@ -0,0 +1,15 @@
+VANTA.BIRDS({
+    el: "#ani",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    backgroundColor: 0x3077c5,
+    color1: 0x8d0000,
+    color2: 0x3838ab,
+    birdSize: 1.50,
+    backgroundAlpha: 0.79
+  })
