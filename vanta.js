8bb522c74ece634f902c36ec7bbedf2a06cff2e5
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-03-06 01:17:11 +0530

    update

--- vanta.js
+++ vanta.js
@@ -1,5 +1,31 @@
-VANTA.BIRDS({
-    el: "#ani",
+// VANTA.BIRDS({
+//     el: ".form-container",
+//     mouseControls: true,
+//     touchControls: true,
+//     gyroControls: false,
+//     minHeight: 200.00,
+//     minWidth: 200.00,
+//     scale: 1.00,
+//     scaleMobile: 1.00,
+//     backgroundColor: 0x3077c5,
+//     color1: 0x8d0000,
+//     color2: 0x3838ab,
+//     birdSize: 1.50,
+//     backgroundAlpha: 0.79
+//   })
+  // VANTA.WAVES({
+  //   el: ".form-container",
+  //   mouseControls: true,
+  //   touchControls: true,
+  //   gyroControls: false,
+  //   minHeight: 200.00,
+  //   minWidth: 200.00,
+  //   scale: 1.00,
+  //   scaleMobile: 1.00,
+  //   color: 0x7a7c75
+  // })
+  VANTA.WAVES({
+    el: ".form-container",
     mouseControls: true,
     touchControls: true,
     gyroControls: false,
@@ -7,9 +33,29 @@ VANTA.BIRDS({
     minWidth: 200.00,
     scale: 1.00,
     scaleMobile: 1.00,
-    backgroundColor: 0x3077c5,
-    color1: 0x8d0000,
-    color2: 0x3838ab,
-    birdSize: 1.50,
-    backgroundAlpha: 0.79
+    color: 0x604949
+  })
+  
+  // VANTA.FOG({
+  //   el: ".form-container",
+  //   mouseControls: true,
+  //   touchControls: true,
+  //   gyroControls: false,
+  //   minHeight: 200.00,
+  //   minWidth: 200.00,
+  //   highlightColor: 0xf7f9f4,
+  //   midtoneColor: 0xc000ff,
+  //   lowlightColor: 0xe830c7,
+  //   baseColor: 0xebedff,
+  //   blurFactor: 0.58,
+  //   speed: 2.40,
+  //   zoom: 1.20
+  // })
+  VANTA.FOG({
+    el: ".form-container",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00
   })
