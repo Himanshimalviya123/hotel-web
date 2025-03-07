1da185ac131ff6478dd2993dfc5fea7183e6bc36
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-03-08 00:10:47 +0530

    update

--- vanta.js
+++ vanta.js
@@ -1,18 +1,16 @@
-// VANTA.BIRDS({
-//     el: ".form-container",
-//     mouseControls: true,
-//     touchControls: true,
-//     gyroControls: false,
-//     minHeight: 200.00,
-//     minWidth: 200.00,
-//     scale: 1.00,
-//     scaleMobile: 1.00,
-//     backgroundColor: 0x3077c5,
-//     color1: 0x8d0000,
-//     color2: 0x3838ab,
-//     birdSize: 1.50,
-//     backgroundAlpha: 0.79
-//   })
+
+VANTA.NET({
+  el: "#ani",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0xd0d65,
+  spacing: 13.00
+})
   VANTA.WAVES({
     el: ".form-container",
     mouseControls: true,
@@ -37,7 +35,7 @@
   // })
   
   VANTA.NET({
-    el: "#connect",
+    el: "#ani",
     mouseControls: true,
     touchControls: true,
     gyroControls: false,
