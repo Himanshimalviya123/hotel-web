99f51f5ceaa50cbf99d95111e856922c42cd018e
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-03-10 17:28:14 +0530

    update

--- vanta.js
+++ vanta.js
@@ -22,18 +22,18 @@ VANTA.NET({
     scaleMobile: 1.00,
     color: 0x7a7c75
   })
-  // VANTA.NET({
-  //   el: "#log",
-  //   mouseControls: true,
-  //   touchControls: true,
-  //   gyroControls: false,
-  //   minHeight: 200.00,
-  //   minWidth: 200.00,
-  //   scale: 1.00,
-  //   scaleMobile: 1.00,
-  //   color: 0x2f2f2b,
-  //   backgroundColor: 0x3c3015
-  // })
+  VANTA.NET({
+    el: "#swiper",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x2f2f2b,
+    backgroundColor: 0x3c3015
+  })
   VANTA.NET({
     el: "#log",
     mouseControls: true,
